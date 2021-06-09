92a5e5d5555538f25e6a6ca1f2f1d3e490516bef
Evgeny Kreger <evgeny_kreger@mail.ru>
2021-06-09 23:47:30 +0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export function Profile() {
+    return (
+        <div className={"content"}>
+            <img
+                src="https://www.lawrencerealestateonline.com/wp-content/uploads/2018/01/cropped-Chicargo-Skyline-1600-x-400-lq.jpg"
+                alt=""/>
+            <div>ava+description</div>
+            <div>My posts</div>
+            <div>New post</div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    )
+}
